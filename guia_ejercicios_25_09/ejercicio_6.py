6ece7b51f70c70fbda39a1e8bb8e93c266f38fe9
lbendersky <lbendersky@uade.edu.ar>
2024-09-25 17:24:29 -0300

    ejercicios v1.1

--- guia_ejercicios_25_09/ejercicio_6.py
+++ guia_ejercicios_25_09/ejercicio_6.py
@@ -4,14 +4,6 @@ conjunto = set(frase.split(" "))
 
 print(conjunto)
 
-conjunto = list(conjunto)
-
-flag = -1
-
-while flag + 1 < len(conjunto) - 1:
-    flag += 1
-    if len(conjunto[flag]) < len(conjunto[flag + 1]):
-        conjunto[flag], conjunto[flag + 1] = conjunto[flag + 1], conjunto[flag]
-        flag = -1
+conjunto = sorted(conjunto, key=len)
 
 print(conjunto)
